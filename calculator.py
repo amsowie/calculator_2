672d8404a596b5722db98fade9feee6c6eede4b1
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-04 14:54:00 -0700

    calculation type

--- calculator.py
+++ calculator.py
@@ -10,15 +10,59 @@ from arithmetic import *
 # Your code goes here
 def input_calc():
     """Get calculator input and put into list
-    
-    Prompts the user to enter some input, 
+
+    Prompts the user to enter some input,
     and split out results into a list"""
     calc_input = []
     calc_input = raw_input("> ").split()
-    print calc_input
-    return None
+    return calc_input
 
 
+def convert_nums_to_float(input_list):
+    """convert numbers to float from string format
+
+    Takes index 1 on and converts all to floats, leaves index 0 as string"""
+    float_list = [input_list[0]]
+
+    for item in range(1,len(input_list)):
+        float_list.append(float(input_list[item]))
+
+    return float_list
 
 
-input_calc()
+def which_calc_type(input_list):
+    """Determines calculation type based on first index
+
+    Compares each first index of user input string to
+    computation symbols and calls proper calculation function"""
+
+    input_list = convert_nums_to_float(input_list)
+
+    if input_list[0] == "+":
+        answer = add(input_list[1], input_list[2])
+    elif input_list[0] == "-":
+        answer = subtract(input_list[1], input_list[2]) 
+    elif input_list[0] == "*":
+        answer = multiply(input_list[1], input_list[2])
+    elif input_list[0] == "/":
+        answer = divide(input_list[1], input_list[2])
+    elif input_list[0] == "square":
+        answer = square(input_list[1])
+    elif input_list[0] == "cube":
+        answer = cube(input_list[1])
+    elif input_list[0] == "pow":
+        answer = power(input_list[1], input_list[2])
+    elif input_list[0] == "mod":
+        answer = mod(input_list[1], input_list[2])
+    elif input_list[0].lower() == "q" or input_list[0].lower() == "quit":
+        answer = None
+    else:
+        print "Entry not valid"
+        answer = None
+
+    return answer
+
+
+calc_list = input_calc()
+
+print which_calc_type(calc_list)
